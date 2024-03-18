e7097b497ea3c6a8476052d06cad8300f769eec7
siganushka <siganushka@gmail.com>
2024-03-18 16:13:25 +0800

    feat: add img url(with hash) compatible for MediaType

--- src/Form/Type/MediaType.php
+++ src/Form/Type/MediaType.php
@@ -8,6 +8,7 @@ use Doctrine\Persistence\ManagerRegistry;
 use Siganushka\GenericBundle\Form\DataTransformer\EntityToIdentifierTransformer;
 use Siganushka\MediaBundle\Entity\Media;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\DataTransformerInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormInterface;
@@ -15,7 +16,7 @@ use Symfony\Component\Form\FormView;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
 
-class MediaType extends AbstractType
+class MediaType extends AbstractType implements DataTransformerInterface
 {
     private ManagerRegistry $registry;
 
@@ -26,8 +27,10 @@ class MediaType extends AbstractType
 
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $builder->addViewTransformer($this, true);
+
         // using addViewTransformer to transformer object for view
-        $builder->addViewTransformer(new EntityToIdentifierTransformer($this->registry, Media::class, 'hash'));
+        $builder->addViewTransformer(new EntityToIdentifierTransformer($this->registry, Media::class, 'hash'), true);
     }
 
     public function buildView(FormView $view, FormInterface $form, array $options): void
@@ -55,5 +58,32 @@ class MediaType extends AbstractType
     public function getParent(): string
     {
         return TextType::class;
+    }
+
+    /**
+     * @param ?string $value
+     */
+    public function transform($value): ?string
+    {
+        return $value;
+    }
+
+    /**
+     * @param ?string $value
+     */
+    public function reverseTransform($value): ?string
+    {
+        if (null === $value) {
+            return null;
+        }
+
+        $queryString = parse_url($value, \PHP_URL_QUERY);
+        if (null === $queryString) {
+            return $value;
+        }
+
+        parse_str($queryString, $result);
+
+        return $result['hash'] ?? $value;
     }
 }
