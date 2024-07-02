4bf5b8d4e1037e13340e240db042435c9d2a5ea2
siganushka <siganushka@gmail.com>
2024-07-02 16:28:58 +0800

    refactor: Using php format replace yaml for translations

---
+++ src/Resources/translations/messages.zh_CN.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'media.hash' => '标识',
+    'media.channel' => '类型',
+    'media.file' => '文件',
+
+    'media.name' => '名称',
+    'media.extension' => '后缀',
+    'media.mime_type' => '类型',
+    'media.size' => '大小',
+    'media.pixel' => '尺寸',
+    'media.image' => '图象',
+];
--- src/Serializer/Normalizer/MediaNormalizer.php
+++ src/Serializer/Normalizer/MediaNormalizer.php
@@ -5,14 +5,17 @@ declare(strict_types=1);
 namespace Siganushka\MediaBundle\Serializer\Normalizer;
 
 use Siganushka\MediaBundle\Entity\Media;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Serializer\Normalizer\AbstractNormalizer;
-use Symfony\Component\Serializer\Normalizer\NormalizerAwareInterface;
-use Symfony\Component\Serializer\Normalizer\NormalizerAwareTrait;
 use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
 
-class MediaNormalizer implements NormalizerInterface, NormalizerAwareInterface
+class MediaNormalizer implements NormalizerInterface
 {
-    use NormalizerAwareTrait;
+    public function __construct(
+        #[Autowire(service: 'serializer.normalizer.object')]
+        private readonly NormalizerInterface $normalizer,
+    ) {
+    }
 
     public function normalize($object, string $format = null, array $context = []): array|string|int|float|bool|\ArrayObject|null
     {
