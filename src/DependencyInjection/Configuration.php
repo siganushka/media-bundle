155c3ea6477f95ec3d64345516b7f183a1c005c0
siganushka <siganushka@gmail.com>
2024-08-31 13:25:25 +0800

    feat: add channels configuration

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -6,9 +6,12 @@ namespace Siganushka\MediaBundle\DependencyInjection;
 
 use Siganushka\MediaBundle\Entity\Media;
 use Siganushka\MediaBundle\Storage\LocalStorage;
+use Siganushka\MediaBundle\Storage\StorageInterface;
 use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\File;
 
 /**
  * @psalm-suppress UndefinedInterfaceMethod
@@ -26,22 +29,63 @@ class Configuration implements ConfigurationInterface
                 ->scalarNode('media_class')
                     ->defaultValue(Media::class)
                     ->validate()
-                        ->ifTrue(function (mixed $v) {
-                            if (!class_exists($v)) {
-                                return false;
-                            }
-
-                            return !is_subclass_of($v, Media::class);
-                        })
-                        ->thenInvalid('The %s class must extends '.Media::class.' for using the "media_class".')
+                        ->ifTrue(static fn (mixed $v): bool => !is_a($v, Media::class, true))
+                        ->thenInvalid('The value must be instanceof '.Media::class.', %s given.')
                     ->end()
                 ->end()
                 ->scalarNode('storage')
                     ->defaultValue(LocalStorage::class)
                     ->cannotBeEmpty()
+                    ->validate()
+                        ->ifTrue(static fn (mixed $v): bool => !is_a($v, StorageInterface::class, true))
+                        ->thenInvalid('The value must be instanceof '.StorageInterface::class.', %s given.')
+                    ->end()
                 ->end()
         ;
 
+        $this->addChannelsSection($rootNode);
+
         return $treeBuilder;
+    }
+
+    public function addChannelsSection(ArrayNodeDefinition $rootNode): void
+    {
+        /** @var ArrayNodeDefinition */
+        $channelsNodeBuilder = $rootNode
+            ->fixXmlConfig('channel')
+            ->children()
+                ->arrayNode('channels')
+                    ->example([
+                        'foo' => [
+                            'constraint_options' => ['maxSize' => '2MB'],
+                        ],
+                        'bar' => [
+                            'constraint' => 'Symfony\Component\Validator\Constraints\Image',
+                            'constraint_options' => ['minWidth' => 320, 'allowSquare' => true],
+                        ],
+                        'baz' => [
+                            'constraint' => 'App\Validator\MyCustomConstraint',
+                            'constraint_options' => ['abc' => 1],
+                        ],
+                    ])
+                    ->prototype('array')
+        ;
+
+        $channelsNodeBuilder
+            ->children()
+                ->scalarNode('constraint')
+                    ->info('This value will be used for validation when uploading files.')
+                    ->defaultValue(File::class)
+                    ->validate()
+                        ->ifTrue(static fn (mixed $v): bool => !is_a($v, Constraint::class, true))
+                        ->thenInvalid('The value must be instanceof '.Constraint::class.', %s given.')
+                    ->end()
+                ->end()
+                ->arrayNode('constraint_options')
+                    ->info('This value will be passed to the validation constraint.')
+                    ->defaultValue([])
+                    ->useAttributeAsKey('name')
+                    ->prototype('variable')
+        ;
     }
 }
---
+++ tests/DependencyInjection/ConfigurationTest.php
@@ -0,0 +1,109 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\MediaBundle\Tests\DependencyInjection;
+
+use PHPUnit\Framework\TestCase;
+use Siganushka\MediaBundle\DependencyInjection\Configuration;
+use Siganushka\MediaBundle\Entity\Media;
+use Siganushka\MediaBundle\Storage\AliyunOssStorage;
+use Siganushka\MediaBundle\Storage\LocalStorage;
+use Siganushka\MediaBundle\Storage\StorageInterface;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Config\Definition\Processor;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\Image;
+
+class ConfigurationTest extends TestCase
+{
+    public function testDefaultConfig(): void
+    {
+        $processor = new Processor();
+        $processedConfig = $processor->processConfiguration(new Configuration(), []);
+
+        static::assertSame($processedConfig, [
+            'media_class' => Media::class,
+            'storage' => LocalStorage::class,
+            'channels' => [],
+        ]);
+    }
+
+    public function testCustomConfig(): void
+    {
+        $config = [
+            'media_class' => FooMedia::class,
+            'storage' => AliyunOssStorage::class,
+            'channels' => [
+                'foo' => [
+                    'constraint_options' => ['maxSize' => '2MB'],
+                ],
+                'bar' => [
+                    'constraint' => Image::class,
+                    'constraint_options' => ['minWidth' => 320, 'allowSquare' => true],
+                ],
+            ],
+        ];
+
+        $processor = new Processor();
+        $processedConfig = $processor->processConfiguration(new Configuration(), [$config]);
+        // dd($processedConfig);
+
+        static::assertSame($processedConfig['media_class'], $config['media_class']);
+        static::assertSame($processedConfig['storage'], $config['storage']);
+
+        static::assertSame($processedConfig['channels']['foo']['constraint'], File::class);
+        static::assertSame($processedConfig['channels']['foo']['constraint_options'], ['maxSize' => '2MB']);
+
+        static::assertSame($processedConfig['channels']['bar']['constraint'], Image::class);
+        static::assertSame($processedConfig['channels']['bar']['constraint_options'], ['minWidth' => 320, 'allowSquare' => true]);
+    }
+
+    public function testMediaClassInvalidException(): void
+    {
+        $this->expectException(InvalidConfigurationException::class);
+        $this->expectExceptionMessage(\sprintf('The value must be instanceof %s, "stdClass" given', Media::class));
+
+        $config = [
+            'media_class' => \stdClass::class,
+        ];
+
+        $processor = new Processor();
+        $processor->processConfiguration(new Configuration(), [$config]);
+    }
+
+    public function testStorageInvalidException(): void
+    {
+        $this->expectException(InvalidConfigurationException::class);
+        $this->expectExceptionMessage(\sprintf('The value must be instanceof %s, "stdClass" given', StorageInterface::class));
+
+        $config = [
+            'storage' => \stdClass::class,
+        ];
+
+        $processor = new Processor();
+        $processor->processConfiguration(new Configuration(), [$config]);
+    }
+
+    public function testChannelsInvalidException(): void
+    {
+        $this->expectException(InvalidConfigurationException::class);
+        $this->expectExceptionMessage(\sprintf('The value must be instanceof %s, "stdClass" given', Constraint::class));
+
+        $config = [
+            'channels' => [
+                'foo' => [
+                    'constraint' => \stdClass::class,
+                ],
+            ],
+        ];
+
+        $processor = new Processor();
+        $processor->processConfiguration(new Configuration(), [$config]);
+    }
+}
+
+class FooMedia extends Media
+{
+}
