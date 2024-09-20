0ccff4a585b0692c4098b087ce663c50de1c80fa
siganushka <siganushka@gmail.com>
2024-09-20 12:00:36 +0800

    refactor: remove invalid namespace

--- config/routes.php
+++ config/routes.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 
-use Siganushka\MediaBundle\SiganushkaMediaBundle;
 use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 return static function (RoutingConfigurator $routes): void {
