238322ed29aef6df20c0769be29892f58da4bda9
siganushka <siganushka@gmail.com>
2024-11-22 14:34:49 +0800

    refactor: improivng repository method return type

--- src/Repository/MediaRepository.php
+++ src/Repository/MediaRepository.php
@@ -9,12 +9,6 @@ use Siganushka\MediaBundle\Entity\Media;
 
 /**
  * @extends GenericEntityRepository<Media>
- *
- * @method Media      createNew(...$args)
- * @method Media|null find($id, $lockMode = null, $lockVersion = null)
- * @method Media|null findOneBy(array $criteria, array $orderBy = null)
- * @method Media[]    findAll()
- * @method Media[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
 class MediaRepository extends GenericEntityRepository
 {
