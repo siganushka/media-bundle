45043eb02091153600fc90f6812555e48221a902
siganushka <siganushka@gmail.com>
2025-07-24 15:46:27 +0800

    feat: add validator trans messages

---
+++ translations/validators.en.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'The value is not a valid media reference.' => 'The value is not a valid media reference.',
+];
---
+++ translations/validators.zh_CN.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'The value is not a valid media reference.' => '该变量不是一个有效的值。',
+];
