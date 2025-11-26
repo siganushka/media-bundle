6549af41530edd9a1a26c394f1434f74f675c424
siganushka <siganushka@gmail.com>
2025-11-26 17:44:38 +0800

    tests: improve test routes

--- tests/RoutesTest.php
+++ tests/RoutesTest.php
@@ -11,13 +11,13 @@ use Symfony\Component\Config\Loader\LoaderResolver;
 use Symfony\Component\Routing\Loader\AttributeDirectoryLoader;
 use Symfony\Component\Routing\Loader\PhpFileLoader;
 use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
 
 class RoutesTest extends TestCase
 {
-    /**
-     * @dataProvider routesProvider
-     */
-    public function testRotues(string $routeName, string $path, array $methods): void
+    protected RouteCollection $routes;
+
+    protected function setUp(): void
     {
         $locator = new FileLocator(__DIR__.'/../config/');
 
@@ -26,9 +26,26 @@ class RoutesTest extends TestCase
             new AttributeDirectoryLoader($locator, new AttributeRouteControllerLoader()),
         ]);
 
-        $routes = $loader->load('routes.php');
+        $this->routes = $loader->load('routes.php');
+    }
+
+    public function testAll(): void
+    {
+        static::assertSame([
+            'siganushka_media_media_getcollection',
+            'siganushka_media_media_postcollection',
+            'siganushka_media_media_getitem',
+            'siganushka_media_media_deleteitem',
+        ], array_keys($this->routes->all()));
+    }
+
+    /**
+     * @dataProvider routesProvider
+     */
+    public function testRotues(string $routeName, string $path, array $methods): void
+    {
         /** @var Route */
-        $route = $routes->get($routeName);
+        $route = $this->routes->get($routeName);
 
         static::assertSame($path, $route->getPath());
         static::assertSame($methods, $route->getMethods());
