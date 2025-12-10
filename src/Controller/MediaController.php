5051b68a1139823a17effea7d152f2f3ecbb6c3c
siganushka <siganushka@gmail.com>
2025-12-10 16:16:44 +0800

    fix: fixed post max upload size message

--- src/Controller/MediaController.php
+++ src/Controller/MediaController.php
@@ -16,14 +16,18 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
 use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Form\Util\FormUtil;
+use Symfony\Component\Form\Util\ServerParams;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryString;
 
 class MediaController extends AbstractController
 {
-    public function __construct(protected readonly MediaRepository $mediaRepository)
+    private readonly ServerParams $serverParams;
+
+    public function __construct(private readonly MediaRepository $mediaRepository)
     {
+        $this->serverParams = new ServerParams();
     }
 
     public function getCollection(PaginatorInterface $paginator, #[MapQueryString] PageQueryDto $dto): Response
@@ -42,6 +46,11 @@ class MediaController extends AbstractController
 
         $form = $this->createForm(MediaUploadType::class);
         $form->submit($formData);
+
+        /* @see HttpFoundationRequestHandler::handleRequest */
+        if ($this->serverParams->hasPostMaxSizeBeenExceeded()) {
+            $form->addError(new FormError($form->getConfig()->getOption('upload_max_size_message')()));
+        }
 
         if (!$form->isValid()) {
             return $this->createFormErrorResponse($form, Response::HTTP_BAD_REQUEST);
