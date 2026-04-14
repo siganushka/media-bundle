72be292c01aeead15774b0a6f8d0cd4ebcec7119
siganushka <siganushka@gmail.com>
2026-04-14 17:10:44 +0800

    feat: add MediaReferenceController to generate media route

---
+++ src/Controller/MediaReferenceController.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Siganushka\MediaBundle\Controller;
+
+use Siganushka\MediaBundle\Repository\MediaRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class MediaReferenceController extends AbstractController
+{
+    #[Route('/m/{hash<[0-9a-fA-F]+>}', name: 'siganushka_media_reference', methods: 'GET')]
+    public function __invoke(Request $request, MediaRepository $repository, string $hash): Response
+    {
+        $entity = $repository->findOneByHash($hash);
+        if (!$entity || !$url = $entity->getUrl()) {
+            throw $this->createNotFoundException();
+        }
+
+        // $url .= sprintf('?t=%d', $entity->getCreatedAt()->getTimestamp());
+
+        $response = new RedirectResponse($url);
+        $response->setPublic();
+        $response->setMaxAge(31536000);
+        $response->setSharedMaxAge(31536000);
+        $response->setLastModified($entity->getCreatedAt());
+        $response->setEtag(md5($url));
+
+        if ($response->isNotModified($request)) {
+            return $response;
+        }
+
+        // do something...
+
+        return $response;
+    }
+}
